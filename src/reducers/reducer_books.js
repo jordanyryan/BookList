f24f303a8bf6adb1a4e2a759c7dbd1d6917ea59a
Jordany Rosas <jordanyryan@gmail.com>
2017-05-11 19:20:33 -0400

    Using reducers/containers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,1 +1,8 @@
-"stuff"
+export default function() {
+  return [
+  {title: "JavaScript: The Good Parts"},
+  {title: "Harry Potter"},
+  {title: "The Dark Tower"},
+  {title: "Eloquent Ruby"}
+  ]
+}
